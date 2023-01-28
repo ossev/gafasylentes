a04c01ad8169c1f08e87c7ac402e14b4d382a74c
Osnayder Severiche <osna.sev@gmail.com>
2023-01-28 18:43:54 -0500

    corrección de la interactividad

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://ossev.github.io",
-  base:"/gafasylentes",
   integrations: [
     tailwind(),
     image({
